e2c084e3774ce76f2ec56debd0e0f72a23232a15
Codaop <syauqya2404@it.student.pens.ac.id>
2026-02-10 23:52:35 +0700

    feat: implement CRUD User controller, implement user DTO and implement user seeder. Added change password feature also for user.

---
+++ Dtos/Users/UserCreateDto.cs
@@ -0,0 +1,8 @@
+namespace BorrowIt.Dtos.Users;
+
+public class UserCreateDto
+{
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+}
---
+++ Dtos/Users/UserReadDto.cs
@@ -0,0 +1,8 @@
+namespace BorrowIt.Dtos.Users;
+
+public class UserReadDto
+{
+    public int Id { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+}
