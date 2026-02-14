5337974727d5954b703fcca4cd48dc5e2faa5092
Codaop <syauqya2404@it.student.pens.ac.id>
2026-02-14 15:34:48 +0700

    refactor(user): changed string type in DTOs.

--- Dtos/Users/UserUpdateDto.cs
+++ Dtos/Users/UserUpdateDto.cs
@@ -2,6 +2,6 @@ namespace BorrowIt.Dtos.Users;
 
 public class UserUpdateDto
 {
-    public required string Username { get; set; }
-    public required string Email { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
 }
