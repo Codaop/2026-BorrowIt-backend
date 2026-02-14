15109f090c03883f5839966d96d9996b1093bc6d
Codaop <syauqya2404@it.student.pens.ac.id>
2026-02-14 19:53:06 +0700

    feat(ruangan): add DTO, mapper and controller for Ruangan update feature.

--- Mappers/Ruangans/RuanganMappers.cs
+++ Mappers/Ruangans/RuanganMappers.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using BorrowIt.Dtos.Ruangans;
 using BorrowIt.Models;
 using Microsoft.Identity.Client;
@@ -26,5 +27,13 @@ public static class RuanganMappers
             Kapasitas = dto.Kapasitas,
             JenisRuangan = dto.JenisRuangan
         };
+    }
+
+    public static void RequestRuanganUpdateDto(this Ruangan ruangan, RuanganUpdateDto dto)
+    {
+        ruangan.NamaRuangan = !string.IsNullOrWhiteSpace(dto.NamaRuangan) ? dto.NamaRuangan : ruangan.NamaRuangan;
+        ruangan.Kapasitas = dto.Kapasitas ?? ruangan.Kapasitas;
+        ruangan.JenisRuangan = !string.IsNullOrWhiteSpace(dto.JenisRuangan) ? dto.JenisRuangan : ruangan.JenisRuangan;
+        if (dto.IsTersedia.HasValue) ruangan.IsTersedia = dto.IsTersedia.Value;
     }
 }
